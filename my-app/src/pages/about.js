1d9fb5f61583743121df980b3632525475c09306
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-03-16 14:14:02 +0000

    added a picture!

--- my-app/src/pages/about.js
+++ my-app/src/pages/about.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import "./about.css"
 
 const About= () =>{
   return (
@@ -7,7 +7,7 @@ const About= () =>{
       <h3>About</h3>
       <div>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <img alt= " of me " src="/Images/329922260_555228413244888_4268009491925438411_n.jpg"/>
+        <img  className="img" alt= " of me " src="/img/circle.png"/>
       </div>
     </div>
   );
