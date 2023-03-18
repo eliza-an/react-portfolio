dc90b2b81ef99fabf59c185e7da69260c747f635
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-03-18 00:44:50 +0000

    added a cv download button

---
+++ my-app/src/components/cvButton/button.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import "./style.css"
+
+let cvBtn=() =>{
+   return( 
+  <button>
+    <a href="my-app\public\Elizaveta Anufrieva.cv.pdf" download="Elizaveta Anufrieva CV">Download CV</a>
+   </button>
+)}
+export default cvBtn;
--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import "./home.css"
 
+
 const About= () =>{
   return (
     <div>
       
-      <div> 
+      
        <h1 className='text1'>Your Next Front-End Web Developer</h1>  
-       <img  className="img" alt= " of me " src="/img/circle.png"/>
+       <button>
+    <a href="/Elizaveta Anufrieva.cv.pdf" download="Elizaveta Anufrieva CV">Download CV</a>
+   </button>
+       <div className='imageDiv'> <img  className="img" alt= " of me " src="/img/circle.png"/></div>
 
        
        
        
-      </div>
+      
     </div>
   );
 }
