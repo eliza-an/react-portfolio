3605433233f90037e1275bdab08cf21650bf9155
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-03-19 23:13:24 +0000

    finished about

--- my-app/src/pages/about.js
+++ my-app/src/pages/about.js
@@ -8,10 +8,18 @@ const About= () =>{
   return (
     <div>
       
-    
-       <h1 className='text1'>Hello</h1> 
+     <div className='imageDiv2'> 
+     <img  className="img2" alt= " of me " src="./img/pic.png"/>
+     <h1 className='about'>About</h1> </div>
+     <p className='twentytwo'>A 22 year old front-end web developer located in Manchester, UK! </p>
+     
+    <p>In July of  2022 graduated from University of Manchester with a 2:1 in Politics, Economics and Philosophy and a first in the dissertation. After this, I began my web development journey through Trilogy Skills Bootcamp. Aiming to finish the bootcamp in March 2023.</p>
+     
+        
       
-       <div className='imageDiv'> <img  className="img2" alt= " of me " src="./img/pic.jpg"/></div>
+       
+      
+      
 
        
        
