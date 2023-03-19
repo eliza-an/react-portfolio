b07dba6b7913143412aa9d6651332b150bc80b12
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-03-19 23:31:10 +0000

    made a card for portfolio

--- my-app/src/pages/portfolio.js
+++ my-app/src/pages/portfolio.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import "./portfolio.css"
 
 
 
 const Portfolio= () =>{
   return (
     <div>
-      <h3>Portfolio</h3>
-      <div>
+      <h3 className='title'>Portfolio</h3>
+      <div className='firstCard'>
+        <h2 className='cardtitle'>RSE Delivered</h2>
+       <div className="imageDiv"> <img  className="computer1" src="/img/computer.jpg"></img></div>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-     asdfghjk
-        
+  
       </div>
     </div>
   );
