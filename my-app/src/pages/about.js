992199e5793f6b0ba915f407d3dabea99356a69d
eliza-an <118762745+eliza-an@users.noreply.github.com>
2023-04-03 15:07:01 +0100

    about page

--- my-app/src/pages/about.js
+++ my-app/src/pages/about.js
@@ -13,7 +13,8 @@ const About= () =>{
      <h1 className='about'>About</h1> </div>
      <p className='twentytwo'>A 22 year old front-end web developer located in Manchester, UK! </p>
      
-    <p>In July of  2022 graduated from University of Manchester with a 2:1 in Politics, Economics and Philosophy and a first in the dissertation. After this, I began my web development journey through Trilogy Skills Bootcamp. Aiming to finish the bootcamp in March 2023.</p>
+    <p>In July of 2022 I graduated from University of Manchester with a 2:1 in Politics, Economics and Philosophy and a first in my dissertation. My love for politics was strong, yet I still craved the satifaction of mathematical problem solving and design that I simply could not get from a political career path. At first I thought tutoring maths part time would be a solution to that problem, but soon enough I came to the conclusion that I had to take that leap of faith and try web development.  </p>
+    <p>After frantically searching for ways to pursue my newfound interest in programming, I stumbled across a Web Developement bootcamp, promising to teach me all the skills necessaryto start my career. So that's what I did! After finishing the bootcamp with a 96.7% (A) I was now certain that this is exactly the sector I want have a carrewer in. I honestly cannot wait for my next opportunity to express and develop these skills. </p>
      
         
       
